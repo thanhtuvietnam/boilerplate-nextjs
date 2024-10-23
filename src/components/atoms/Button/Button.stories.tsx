a5479a80d12a7299aa007102b314cfef94fd0c52
thanhtuvietnam <luuthanhtung555@gmail.com>
2024-10-23 22:56:34 +0700

    feat(setup): testing, formating, linting, commiting features for the start of the project

---
+++ src/components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+  title: 'Atoms/Button',
+  component: Button,
+  tags: ['autodocs'],
+  args: {
+    children: 'Example',
+    variant: 'default',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {};
---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,5 @@
+export const Button = () => (
+  <div>
+    <h1>Button</h1>
+  </div>
+);
