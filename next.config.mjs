abb8faee63f72d5a24f238532f0c9584cbf0354f
thanhtuvietnam <luuthanhtung555@gmail.com>
2024-10-25 05:32:40 +0700

    fix: adding .gitkeep

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: {
+    appIsrStatus: true, // defaults to true
+    buildActivity: true, // defaults to true
+    buildActivityPosition: 'bottom-right',
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+      hmrRefreshes: true,
+    },
+  },
+  poweredByHeader: false,
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: [],
+  },
 };
 
 export default nextConfig;
